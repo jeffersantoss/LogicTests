eba45e43753c65b4d8e56713592e85c3109021b8
Jefferson Santos <jefferson.passos@iatec.com>
2022-12-15 12:24:47 -0300

    Add project files.

---
+++ LogicTests/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//5
+//4
+//2 1 3 2 4
+//2 0 2 0 1
+//6 2 1 3 0
+//3 1 0 4 3
+
+using LogicTests.Questions;
+
+List<List<int>> forest = new()
+{
+    new () { 2, 1, 3, 2, 4},
+    new () { 2, 0, 2, 0, 1},
+    new () { 6, 2, 1, 3, 0},
+    new () { 3, 1, 0, 4, 3},
+};
+
+// Sould return 12
+Console.WriteLine($"The busy beaver collected {BusyBeaver.LogsCalculate(forest)} logs.");
---
+++ LogicTests/Questions/BusyBeaver.cs
@@ -0,0 +1,93 @@
+﻿using System.Text;
+
+namespace LogicTests.Questions;
+
+public static class BusyBeaver
+{
+    private static int _logsAmount;
+    private static int _rowIndex;
+    private static int _colIndex;
+
+    public static int LogsCalculate(List<List<int>> forest)
+    {
+        SetBeaverPosition(forest);
+
+        do
+        {
+            CollectLog(forest, _rowIndex, _colIndex);
+            ShowMatrix(forest);
+
+        } while (BeaverCanMove(forest));
+        
+        return _logsAmount;
+    }
+
+    private static bool BeaverCanMove(List<List<int>> forest)
+    {
+        int? upValue = _rowIndex != 0 ? forest[_rowIndex - 1][_colIndex] : 0;
+        int? downValue = _rowIndex < forest.Count - 1 ? forest[_rowIndex + 1][_colIndex] : 0;
+        int? leftValue = _colIndex != 0 ? forest[_rowIndex][_colIndex - 1] : 0;
+        int? rightValue = _colIndex < forest[0].Count - 1 ? forest[_rowIndex][_colIndex + 1] : 0;
+
+        if (upValue == 0 && downValue == 0 && leftValue == 0 && rightValue == 0)
+            return false;
+
+        if (upValue > downValue && upValue > leftValue && upValue > rightValue)
+        {
+            _rowIndex--;
+            return true;
+        }
+
+        if (downValue > upValue && downValue > leftValue && downValue > rightValue)
+        {
+            _rowIndex++;
+            return true;
+        }
+
+        if (leftValue > upValue && leftValue > downValue && leftValue > rightValue)
+        {
+            _colIndex--;
+            return true;
+        }
+
+        if (rightValue > upValue && rightValue > downValue && rightValue > leftValue)
+        {
+            _colIndex++;
+            return true;
+        }
+
+        return false;
+    }
+
+    private static void SetBeaverPosition(List<List<int>> forest)
+    {
+        _rowIndex = Convert.ToInt32(Math.Floor(forest.Count / 2.00));
+        _colIndex = Convert.ToInt32(Math.Floor(forest[0].Count / 2.00));
+
+        if (forest[_rowIndex][_colIndex] < forest[_rowIndex][_colIndex - 1])
+            _colIndex--;
+
+        if (forest[_rowIndex][_colIndex] < forest[_rowIndex - 1][_colIndex])
+            _rowIndex--;
+    }
+
+    private static void CollectLog(List<List<int>> forest, int rowIndex, int colIndex)
+    {
+        _logsAmount += forest[rowIndex][colIndex];
+        forest[rowIndex][colIndex] = 0;
+    }
+
+    private static void ShowMatrix(List<List<int>> forest)
+    {
+        StringBuilder matrix = new();
+        for (int r = 0; r < forest.Count; r++)
+        {
+            for (int c = 0; c < forest[0].Count; c++)
+            {
+                matrix.Append(forest[r][c]);
+            }
+            matrix.Append(Environment.NewLine);
+        }
+        Console.WriteLine(matrix);
+    }
+}
